1db24b521c23a2e2b3b8bcffde1cc8d55e6f5525
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-12-18 11:05:15 +0300

    :lipstick: added social icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,21 @@
 import * as React from "react";
 import "./footer.css";
+import Twitter from "../images/social/twitter.svg";
+import LinkedIn from "../images/social/linkedin.svg";
+import Github from "../images/social/github.svg";
 
 const Footer = () => {
     return (
         <section>
             <div className="footer-container">
-                <h2>Footer</h2>
+                <div className="footer-content">
+                    <h2>Lets get in touch!</h2>
+                    <div className="social-icons">
+                        <button onClick="#"><img src={Twitter}/></button>
+                        <button onClick="#"><img src={LinkedIn}/></button>
+                        <button onClick="#"><img src={Github}/></button>
+                    </div>
+                </div>
             </div>
         </section>
     );
