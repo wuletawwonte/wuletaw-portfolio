7dad4d52118c700296c24c9ba4478f9aa346df31
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-12-18 21:03:07 +0300

    :recycle: footer spacing fixed

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,18 +3,33 @@ import "./footer.css";
 import Twitter from "../images/social/twitter.svg";
 import LinkedIn from "../images/social/linkedin.svg";
 import Github from "../images/social/github.svg";
+import Telegram from "../images/social/telegram.svg";
 
 const Footer = () => {
     return (
         <section>
             <div className="footer-container">
                 <div className="footer-content">
-                    <h2>Lets get in touch!</h2>
-                    <div className="social-icons">
-                        <button onClick="#"><img src={Twitter}/></button>
-                        <button onClick="#"><img src={LinkedIn}/></button>
-                        <button onClick="#"><img src={Github}/></button>
+                    <div className="footer-upper">
+                        <h2>Lets get in touch!</h2>
+                        <div className="social-icons">
+                            <button onClick="#">
+                                <img src={Twitter} />
+                            </button>
+                            <button onClick="#">
+                                <img src={LinkedIn} />
+                            </button>
+                            <button onClick="#">
+                                <img src={Github} />
+                            </button>
+                            <button onClick="#">
+                                <img src={Telegram} />
+                            </button>
+                        </div>
                     </div>
+                    <span>
+                        Designed and developed by: <b>Wuletaw Wonte</b>
+                    </span>
                 </div>
             </div>
         </section>
