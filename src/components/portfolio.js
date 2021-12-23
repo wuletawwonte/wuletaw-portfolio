aa8034459db20027abad2a6c8af1ba73c9b48c58
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-12-23 23:33:56 +0300

    :construction: working on the design of portfolio section

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,13 +1,14 @@
-import * as React from 'react';
+import * as React from "react";
 import "./portfolio.css";
 
 const Portfolio = () => {
     return (
-        <div>
-            <h2>This is Portfolio Section</h2>
+        <div className="portfolio-container">
+            <div className="portfolio-content">
+                <h2>This is Portfolio Section</h2>
+            </div>
         </div>
-    )
-}
-
+    );
+};
 
 export default Portfolio;
