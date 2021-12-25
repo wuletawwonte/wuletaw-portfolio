e5419b20a39a660dd972ad8a2b15bdfa3210a7a2
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-12-25 22:49:54 +0300

    :lipstick: added helmet title tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,15 @@ import HeroSection from "../components/hero";
 import Articles from "../components/articles";
 import Footer from "../components/footer";
 import Portfolio from "../components/portfolio";
+import { Helmet } from "react-helmet";
 
 const IndexPage = () => {
     return (
         <div className="main-content">
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Wuletaw Wonte</title>
+            </Helmet>
             {/* Hero section  */}
             <HeroSection />
             {/* Articles section */}
