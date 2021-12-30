eb16fdd002b7067efa2bf81a14a6c26330abff4a
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-12-30 22:42:40 +0300

    :lipstick: portfolio styled one more time IGUESS

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -6,7 +6,7 @@ const Portfolio = () => {
         <div className="portfolio-container">
             <div className="portfolio-content">
                 <div className="left-portfolio">
-                    <h2>My Software Projects</h2>
+                    <h2>My Software Project</h2>
                 </div>
                 <div className="portfolio-list"></div>
             </div>
